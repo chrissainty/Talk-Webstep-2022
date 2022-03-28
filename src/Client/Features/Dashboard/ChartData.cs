cdf63f8572f53c685b28d21ea7bce8d2cd2d4ebf
Chris Sainty <chris@chrissainty.com>
2022-03-28 12:05:40 +0200

    Initial commit

---
+++ src/Client/Features/Dashboard/ChartData.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazoredRepairs.Client.Features.Dashboard;
+
+public class ChartData
+{
+    public List<string> Labels { get; set; } = new List<string>();
+    public List<DataSet> Datasets { get; set; } = new List<DataSet>();
+}
+
+public class DataSet
+{
+    public string? Label { get; set; }
+    public List<int> Data { get; set; } = new List<int>();
+    public List<string> BackgroundColor { get; set; } = new List<string>();
+    public List<string> BorderColor { get; set; } = new List<string>();
+    public int BorderWidth { get; set; }
+}
---
+++ src/Server/Program.cs
@@ -0,0 +1,57 @@
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Reflection;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["JwtIssuer"],
+                        ValidAudience = builder.Configuration["JwtAudience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSecurityKey"]))
+                    };
+                });
+builder.Services.AddMvc().AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.Load("BlazoredRepairs.Shared")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
